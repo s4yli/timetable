510e41639bc44e1cc5efa02d1be679183872f620
Justine Bachelard <justine.bachelard@be-ys.cloud>
2025-01-14 21:59:29 +0100

    moved to /

--- flask_base/src/helpers/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
-
-
-# db and app are here to avoid circular dependencies
-db = SQLAlchemy()
-app = Flask(__name__)
---
+++ internal/services/collections/service.go
@@ -0,0 +1,45 @@
+package collections
+
+import (
+	"database/sql"
+	"github.com/gofrs/uuid"
+	"github.com/sirupsen/logrus"
+	"middleware/example/internal/models"
+	repository "middleware/example/internal/repositories/collections"
+	"net/http"
+)
+
+func GetAllCollections() ([]models.Collection, error) {
+	var err error
+	// calling repository
+	collections, err := repository.GetAllCollections()
+	// managing errors
+	if err != nil {
+		logrus.Errorf("error retrieving collections : %s", err.Error())
+		return nil, &models.CustomError{
+			Message: "Something went wrong",
+			Code:    500,
+		}
+	}
+
+	return collections, nil
+}
+
+func GetCollectionById(id uuid.UUID) (*models.Collection, error) {
+	collection, err := repository.GetCollectionById(id)
+	if err != nil {
+		if err.Error() == sql.ErrNoRows.Error() {
+			return nil, &models.CustomError{
+				Message: "collection not found",
+				Code:    http.StatusNotFound,
+			}
+		}
+		logrus.Errorf("error retrieving collections : %s", err.Error())
+		return nil, &models.CustomError{
+			Message: "Something went wrong",
+			Code:    500,
+		}
+	}
+
+	return collection, err
+}
